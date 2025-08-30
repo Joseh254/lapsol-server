4042ab953f92723e99a6a79fec2bc92e0f289dd0
Joseh254 <josephkarimambugua@gmail.com>
2025-08-30 12:55:05 +0300

    work on login controller

--- Controllers/LoginController/Login.Controller.js
+++ Controllers/LoginController/Login.Controller.js
@@ -1,3 +1,34 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient();
+
 export async function LoginController(request, response) {
-  response.send("login in user");
+ const { email, password} = request.body;
+
+ try {
+  const userExists = await prisma.users.findFirst({where:{email:email}})
+  if(!userExists){return response.status(404).json({success:false, message:"Wrong credentials!"})}
+  const passwordMatch = bcrypt.compareSync(password,userExists.password)
+  if(!passwordMatch){return response.status(400).json({success:false,message:"Wrong credentials"})}
+  else{
+    const payload = {
+      id:userExists.id,
+      username:userExists.username,
+      firstname:userExists.firstname,
+      lastname:userExists.lastname,
+      phonenumber:userExists.phonenumber,
+      role:userExists.role,
+    }
+    const token = jwt.sign(payload,process.env.JWT_SECRET)
+    response.cookie('access_token',token).json({success:true,message:"Logged in succesfully",data:payload})
+  }
+  
+ } catch (error) {
+  console.log("error loging in user",error.message);
+  return response.status(500).json({success:false,message:"Internal server error!"})
+  
+ }
 }
--- Controllers/SignUp/SignUp.Controller.js
+++ Controllers/SignUp/SignUp.Controller.js
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-
+import bcrypt from 'bcrypt'
 const prisma = new PrismaClient();
 
 export async function SignUpController(request, response) {
   const { email, firstname, lastname, username, phonenumber, password } =
     request.body;
+
+    const hashedPassword = bcrypt.hashSync(password,10)
 
   try {
     const createdUser = await prisma.users.create({
@@ -14,8 +16,18 @@ export async function SignUpController(request, response) {
         lastname,
         username,
         phonenumber,
-        password,
+        password:hashedPassword,
       },
+      select:{
+        email:true,
+        firstname:true,
+        lastname:true,
+        username:true,
+        phonenumber:true,
+        role:true,
+        createdAt: true,
+        updatedAt: true,
+      }
     });
 
     response.status(200).json({
