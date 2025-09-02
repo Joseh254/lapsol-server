1c228daacbadae9c34363a995d1afa7ec73ae1fd
Joseh254 <josephkarimambugua@gmail.com>
2025-09-02 11:33:06 +0300

    work on getting all products controller

--- Controllers/FetchAllProducts/FetchAllProducts.Controller.js
+++ Controllers/FetchAllProducts/FetchAllProducts.Controller.js
@@ -3,5 +3,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function FetchAllProductsController(request, response) {
-  response.send("fetchin all products");
+  try {
+    const products = await prisma.products.findMany();
+    if (products.length == 0) {
+      return response
+        .status(404)
+        .json({ success: false, message: "No products found" });
+    }
+    return response.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error fetching all products", error.message);
+    response
+      .status(500)
+      .json({ success: false, messgae: "internal server error!" });
+  }
 }
