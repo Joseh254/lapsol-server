a8ea0e2c5f865f1fac7757a4c7508ea01c46c754
Joseh254 <josephkarimambugua@gmail.com>
2025-09-04 14:53:29 +0300

    implement getting all customers

--- Controllers/FetchAllCustomers/FetchAllCustomers.Controller.js
+++ Controllers/FetchAllCustomers/FetchAllCustomers.Controller.js
@@ -3,5 +3,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function FetchAllCustomersController(request, response) {
-  response.send("fetching all customers");
+  try {
+    const AllCustomers = await prisma.customers.findMany();
+    if (AllCustomers.length == 0) {
+      return response
+        .status(404)
+        .json({ success: false, message: "No customers found" });
+    }
+    response.status(200).json({ success: true, data: AllCustomers });
+  } catch (error) {
+    console.log("error fetching all customers", error.message);
+    return response
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 }
