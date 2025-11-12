61700af387ba9af86f6b9e3126fe453600a30646
Joseh254 <josephkarimambugua@gmail.com>
2025-11-12 15:41:46 +0300

    add user auth middleware

--- Mpesa/Routes/SendStkPushRoute/SendStkPush.Route.js
+++ Mpesa/Routes/SendStkPushRoute/SendStkPush.Route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { GetTokenMiddleware } from "../../Middleware/GetTokenMiddleware/GetToken.Middleware.js";
 import { sendStkPush } from "../../Controller/StkPushController/StkPush.Controller.js";
-import 
+import { UserAuth } from "../../../Auths/User/UserAuth.js";
 const router = Router();
 
-router.post("/stkPush", GetTokenMiddleware, sendStkPush);
-export default 
+router.post("/stkPush", UserAuth,GetTokenMiddleware, sendStkPush);
+export default router;
